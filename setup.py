5363846cf812785c722bb2e51c4beec2485a3de1
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2018-09-03 09:12:56 +0200

    publish 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name         = 'elastico',
-    version      = '0.0.5',
+    version      = '0.0.6',
     description  = "Elasticsearch Companion - a commandline tool",
     author       = "Kay-Uwe (Kiwi) Lorenz",
     author_email = "kiwi@franka.dyndns.org",
